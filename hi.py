cb2094f039bc9f4f5fd55c17641bbc433c388990
eldos24.10 <eldos24.10@mail.ru>
2018-05-25 17:47:57 +0600

    Добавил функционал который записывает данные и читает с этих файлов.

--- hi.py
+++ hi.py
@@ -1,5 +1,13 @@
-first_name = input("What is your name? ")
-last_name = input("What is your last name? ")
+first_name = input("What is your name? ").title()
+last_name = input("What is your last name? ").title()
+full_name = first_name + " " + last_name
 
-print(first_name)
-print(last_name)
+file_name = 'data_of_user.json'
+with open(file_name, 'a') as data:
+    data.write("\n"+full_name)
+
+with open(file_name, 'r') as file_object:
+    user_name = file_object.readlines()
+
+for line in user_name:
+    print(line.strip())
